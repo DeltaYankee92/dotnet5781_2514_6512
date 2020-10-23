d5cc5b46cae7cbf7d417f9dbbe6ff022e28202de
Eliyahu Rosenfeld <eliyahu.rosenfeld@gmail.com>
2020-10-23 16:37:42 +0300

    welcome, 5124

---
+++ dotnet5781_2514_6512/dotnet5781_00_2514_6512/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5781_00_2514_6512
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            newproject();
+            Wellcome5124();
+            Console.ReadKey();
+        }
+
+        static partial void Wellcome5124();
+        private static void newproject()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
