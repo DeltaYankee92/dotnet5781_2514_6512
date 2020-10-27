8686f008e981dda8c6aadcffca70e4c94251236b
eliyahu <eliyahu.rosenfeld@gmail.com>
2020-10-27 19:19:09 +0200

    base for program

---
+++ dotnet5781_2514_6512/dotnet5781_00_2514_6512/Buses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5781_00_2514_6512
+{
+    class Buses
+    {
+        int[] license_plate; //not determining the size here, because there are 2 options. we will add the '-'s to the print function. 
+        int milage;
+        int milage_total; // kilometrag'
+
+        const int max_fuel = 1200;// to use as we go along: maximum fuel
+        int current_fuel;
+        DateTime registrationDate, lastMaintenance; // as requested.
+
+        internal Buses()
+        {
+
+        }
+
+    }
+}
---
+++ dotnet5781_2514_6512/dotnet5781_00_2514_6512/buses main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5781_00_2514_6512
+{
+    class buses_main
+    {
+        const int max_fuel = 1200;// to use as we go along: maximum fuel
+        enum CHOICE { EXIT, ADD, DRIVE, REFUEL, REPAIRS, MILEAGE };
+
+        private static List<Buses> database = new List<Buses>(); // again, as requested
+
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            CHOICE choice;
+            do
+            {
+                bool parse_success = true;
+                parse_success = Enum.TryParse(Console.ReadLine(), out choice);
+                if (!parse_success)
+                    continue;
+                switch(choice)
+                {
+                    case CHOICE.ADD:
+                         ADD();
+                        break;
+                    case CHOICE.DRIVE:
+                         DRIVE(); 
+                        break;
+                    case CHOICE.REFUEL:
+                         REFEUL();
+                        break;
+                    case CHOICE.REPAIRS:
+                         REPAIRS(); 
+                        break;
+                    case CHOICE.MILEAGE:
+                         MILEAGE();
+                        break;
+                    case CHOICE.EXIT:
+                        flag = true;
+                        break;
+                    default:
+                        Console.WriteLine("please try again");
+                        break;
+                }
+
+            } while (flag != true);
+
+    }
+
+        private static void MILEAGE()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void REPAIRS()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void REFEUL()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DRIVE()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ADD()
+        {
+            throw new NotImplementedException();
+        }
+    }
