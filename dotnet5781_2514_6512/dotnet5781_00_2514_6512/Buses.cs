c2f609987d7f5e579fc9a85c7f1fe3f520c41bc7
daniel yanovsky <daniel@yanovsky.me>
2020-11-02 00:08:33 +0200

    last (hoplfly)

--- dotnet5781_2514_6512/dotnet5781_00_2514_6512/Buses.cs
+++ dotnet5781_2514_6512/dotnet5781_00_2514_6512/Buses.cs
@@ -54,14 +54,17 @@ namespace dotnet5781_00_2514_6512
         {
             if ((LastMaintenance+amount_to_drive) >= (LastMaintenance+20000))
             {
+                Console.WriteLine("Bus needs maintenance!");
                 return false;
             }
             if (current_fuel<amount_to_drive)
             {
+                Console.WriteLine("Not enough fuel!");
                 return false;
             }
             if (current_fuel-amount_to_drive < 0)
             {
+                Console.WriteLine("Not enough fuel!");
                 return false;
             }
             return true;
@@ -71,7 +74,7 @@ namespace dotnet5781_00_2514_6512
         {
             milage += amount_to_drive;
             milage_total += amount_to_drive;
-            current_fuel += amount_to_drive;
+            current_fuel -= amount_to_drive;
         }
 
         internal void print(Buses bus)
